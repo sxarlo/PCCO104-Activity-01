fb9b5eb28f06993a20fcff5c64343bb38ad5a13a
diazsean78 <31803614-diazsean78@users.noreply.replit.com>
2024-02-03 02:44:14 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    string Fname = "Sean";	
+    string greeting = "Hello, my name is";
+    Console.WriteLine (greeting + " " + Fname);
+  }
+}
