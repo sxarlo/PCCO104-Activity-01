1a1c331fc2babebcd9b55cfb03af6adc5640ed73
diazsean78 <31803614-diazsean78@users.noreply.replit.com>
2024-02-03 02:47:49 +0000

    added last name

--- main.cs
+++ main.cs
@@ -3,8 +3,9 @@ using System;
 class Program {
   public static void Main (string[] args) {
 
-    string Fname = "Sean";	
+    string Fname = "Sean";
+    string Lname = "Diaz";
     string greeting = "Hello, my name is";
-    Console.WriteLine (greeting + " " + Fname);
+    Console.WriteLine (greeting + " " + Fname + " " + Lname);;
   }
 }
